08da6ec6b6cdb802c2bf83f9ed6f32b25832284e
Amirhosein Gharaati <amirgh1380@gmail.com>
2022-06-14 22:40:27 +0430

    prettier fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,9 +24,7 @@ export default {
   css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-    '~/plugins/repositories.js'
-  ],
+  plugins: ['~/plugins/repositories.js'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
