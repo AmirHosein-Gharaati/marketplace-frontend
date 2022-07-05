dc758026afdf0b29f943c923b5a5245bae5889a2
Amirhosein Gharaati <amirgh1380@gmail.com>
2022-07-05 22:17:36 +0430

    dark:false

--- nuxt.config.js
+++ nuxt.config.js
@@ -52,7 +52,7 @@ export default {
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
     theme: {
-      dark: true,
+      dark: false,
       themes: {
         dark: {
           primary: colors.blue.darken2,
