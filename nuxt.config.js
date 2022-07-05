a8d30512bf60efaef06f8e7cae04a44fd453c7c3
Amirhosein Gharaati <amirgh1380@gmail.com>
2022-07-05 20:52:00 +0430

    config for scss

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: ['~/assets/styles/app.scss'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: ['~/plugins/repositories.js'],
@@ -51,7 +51,6 @@ export default {
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
-    customVariables: ['~/assets/variables.scss'],
     theme: {
       dark: true,
       themes: {
