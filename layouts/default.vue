bbf3217f146d105b3ff08754f8d8af8668a66b6a
Amirhosein Gharaati <amirgh1380@gmail.com>
2022-07-05 22:19:04 +0430

    fixed navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,6 @@
 <template>
   <v-app dark>
     <v-app-bar :clipped-left="clipped" fixed app>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
       <v-toolbar-title v-text="title" />
       <v-spacer />
     </v-app-bar>
