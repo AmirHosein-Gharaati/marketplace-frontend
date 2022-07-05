d2d2c34a4c50a12cf54a9562bcbf04fb27130dec
Amirhosein Gharaati <amirgh1380@gmail.com>
2022-07-05 23:38:57 +0430

    refactor

--- pages/index.vue
+++ pages/index.vue
@@ -2,16 +2,16 @@
   <div class="home">
 
     <div class="container">
-      <section class="main">
-        <img class="main__img" src="~/static/home.png" alt="home" />
-        <div class="main__text txt">
+      <section class="home__main">
+        <img class="home__main__img" src="~/static/home.png" alt="home" />
+        <div class="home__main__text txt">
           <h2>It’s a lot easier to shop from home!</h2>
         </div>
       </section>
     </div>
 
     <div class="container">
-      
+      <section></section>
     </div>
   </div>
 </template>
