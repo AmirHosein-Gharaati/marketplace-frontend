59c2f1066a1562daf4bfdf803ba35fb4c0d5c26a
Amirhosein Gharaati <amirgh1380@gmail.com>
2022-07-07 10:28:46 +0430

    products pages template

---
+++ pages/products/_id.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container px-16">
+    <h1>{{ id }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProductId',
+  asyncData({ params, redirect }) {
+    return {
+      id: params.id,
+    }
+  },
+}
+</script>
+
+<style></style>
---
+++ pages/products/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>Products Page</h2>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
