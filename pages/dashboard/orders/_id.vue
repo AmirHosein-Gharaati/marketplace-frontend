35991fc04538dae60e6b49bf55996ba1ef7682ea
Amirhosein Gharaati <amirgh1380@gmail.com>
2022-07-07 22:52:56 +0430

    refactor

---
+++ pages/dashboard/orders/_id.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h2>Test</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Order',
+  layout: 'dashboard',
+}
+</script>
---
+++ pages/dashboard/orders/index.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="container px-16">
+    <div class="orders container">
+      <div class="orders__list">
+        <Order
+          v-for="item in orderItems"
+          :key="item.id"
+          :id="item.id"
+          :date="item.date"
+          :price="item.price"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Order from '@/components/Order.vue'
+export default {
+  name: 'Orders',
+  layout: 'dashboard',
+  components: {
+    Order,
+  },
+  data() {
+    return {
+      orderItems: [
+        {
+          id: 1,
+          date: '2018 Jan 19 12:38 PM',
+          price: '298000 Rials',
+        },
+        {
+          id: 2,
+          date: '2018 Jan 19 12:38 PM',
+          price: '298000 Rials',
+        },
+        {
+          id: 3,
+          date: '2018 Jan 19 12:38 PM',
+          price: '298000 Rials',
+        },
+        {
+          id: 4,
+          date: '2018 Jan 19 12:38 PM',
+          price: '298000 Rials',
+        },
+        {
+          id: 5,
+          date: '2018 Jan 19 12:38 PM',
+          price: '298000 Rials',
+        },
+        {
+          id: 6,
+          date: '2018 Jan 19 12:38 PM',
+          price: '298000 Rials',
+        },
+        {
+          id: 7,
+          date: '2018 Jan 19 12:38 PM',
+          price: '298000 Rials',
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style></style>
