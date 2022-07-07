e2060539ff95c886d7cecc259de60ba242287186
Amirhosein Gharaati <amirgh1380@gmail.com>
2022-07-07 22:51:17 +0430

    first impl of orders

--- components/Order.vue
+++ components/Order.vue
@@ -1,13 +1,21 @@
 <template>
-  <div class="order">
-    order item
+  <div class="order" @click="navigate()">
+    <div class="order__id">
+      <h3>{{ id }}</h3>
+    </div>
+    <div class="order__date">{{ date }}</div>
+    <div class="order__price">{{ price }}</div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Order',
+  props: ['id', 'date', 'price'],
+  methods: {
+    navigate() {
+      this.$router.push(`/dashboard/orders/${this.id}`)
+    },
+  },
 }
 </script>
-
-<style></style>
