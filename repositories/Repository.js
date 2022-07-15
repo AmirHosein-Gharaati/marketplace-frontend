c48f80e4073b1f780c72e93d1993e06259ac0dec
Amirhosein Gharaati <amirgh1380@gmail.com>
2022-07-15 09:49:52 +0430

    store repository impl

--- repositories/Repository.js
+++ repositories/Repository.js
@@ -7,6 +7,7 @@ import VoteRepository from './repos/VoteRepository'
 import AddressRepository from './repos/AddressRepository'
 import NotificationRepository from './repos/NotificationRepository'
 import TicketRepository from './repos/TicketRepository'
+import StoreRepository from './repos/StoreRepository'
 
 export default ({ $axios, $axiosClient }) => {
   return {
@@ -19,5 +20,6 @@ export default ({ $axios, $axiosClient }) => {
     address: AddressRepository($axiosClient),
     notification: NotificationRepository($axiosClient),
     ticket: TicketRepository($axiosClient),
+    store: StoreRepository($axiosClient),
   }
 }
